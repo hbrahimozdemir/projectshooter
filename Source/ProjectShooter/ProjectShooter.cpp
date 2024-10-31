ce6eaf414aad36bc6d3dfabd796b3593b422abf3
Halil İbrahim Özdemir <hbrahim.ozdemir@gmail.com>
2024-10-31 20:00:00 +0300

    Empty Project Created

---
+++ Source/ProjectShooter/ProjectShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectShooter, "ProjectShooter" );

a81b132358f2969fec175676db468377c48eb60b
Halil İbrahim Özdemir <hbrahim.ozdemir@gmail.com>
2024-11-01 15:52:43 +0300

    weapon class created

---
+++ Source/ProjectShooter/Public/Weapon/Weapon.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class PROJECTSHOOTER_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+protected:
+	
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Categort="Weapon Properties")
+	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Categort="Weapon Properties")
+	class USphereComponent* WeaponSphere;
+
+
+};
